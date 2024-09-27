8063fc7ca0ca0fdc431194c745a0e345a35df46d
Анна Крохалева <akrohaleva67@gmail.com>
2024-09-27 13:36:24 +0300

    last iter

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -18,6 +18,3 @@ class IsAuthenticatedAndOwnerOrReadOnly(BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.is_authenticated
-
-    def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS
